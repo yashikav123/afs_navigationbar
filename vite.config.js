02167a28c2ee45400d852950f2f08071299bd298
yashikav123 <yashikavasu2004@gmail.com>
2025-01-06 11:37:39 +0530

    fs2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNRESOLVED_IMPORT') return;
+        warn(warning);
+      },
+      external: ['react-router-dom'], // Adding react-router-dom as an external dependency
+    },
+  },
 })
